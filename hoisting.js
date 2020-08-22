4913093510866bfad5222e0510f4ff842dcd4962
dantemessy <thepunishar1@gmail.com>
2020-08-22 17:58:42 +0300

    hoisting updated

--- hoisting.js
+++ hoisting.js
@@ -1,3 +1,9 @@
+'use strict';
+
+
+// variable and function (( declarations )) are put into memory during the compile phase.
+// the value is not hoisted with the declaration.
+
 var foo = 1;
 
 
@@ -6,5 +12,12 @@ var foobar = function() {
   var foo = 2;
 }
 
-
 foobar()
+
+
+
+
+// using let lead us to temporal dead zone;
+// console.log(hoist)
+// var hoist = "value" // undefined
+// let hoist = "value" // expected undefined but it throws as error
