275702268ef66377a5fc74e62c1d3bcdca1415d2
dantemessy <thepunishar1@gmail.com>
2020-08-26 01:09:12 +0300

    promise

--- promise.js
+++ promise.js
@@ -1,10 +1,13 @@
 'use strict';
 
 Promise.resolve(1)
-    .then((x) => x+1)
-    .then((x)=> {throw new Error("my error")})
-    .catch(()=> 1)
-    .then((x) => x+1)
-    .then((x) => console.log(x))
+    .then((x) => x+1) // return 2
+    .then((x)=> {throw new Error("my error")}) // return error
+    .catch(()=> 1) // return 1
+    .then((x) => x+1) // return 2
+    .then((x) => console.log(x)) // x = 2
     .catch(console.error)
 
+
+
+    // result that x = 2 ;
