8f38bfcc9bd5cb9afef12480c6f91d1e7dbd784f
dantemessy <thepunishar1@gmail.com>
2020-11-07 14:20:35 +0200

    destructuring for objects

--- destructuring.js
+++ destructuring.js
@@ -6,8 +6,14 @@
 let obj = { name: 'dante', age: '22', weapons: { sword: 'rebellion', gun: 'Ebony & Ivory' } };
 
 
-let { name: myName, age, weapons: { sword:mySword, gun } } = obj;
-
+let { name: myName, age:myAge, weapons: { sword:mySword, gun } } = obj;
 console.log(myName);
-console.log(age);
+console.log(myAge);
 console.log(mySword, gun);
+
+// if the variables already declared
+let name = 'nero', 
+    age = 12;
+
+({name , age } = obj) // using parenthesis because js take the left side as block; 
+console.log(name, age);
