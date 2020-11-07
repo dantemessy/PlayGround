8fb35e546b191241ccc0b3cc9b9837d2d744eaf8
dantemessy <thepunishar1@gmail.com>
2020-11-07 14:24:32 +0200

    destructuring for arrays

--- destructuring.js
+++ destructuring.js
@@ -6,14 +6,26 @@
 let obj = { name: 'dante', age: '22', weapons: { sword: 'rebellion', gun: 'Ebony & Ivory' } };
 
 
-let { name: myName, age:myAge, weapons: { sword:mySword, gun } } = obj;
+let { name: myName, age: myAge, weapons: { sword: mySword, gun } } = obj;
 console.log(myName);
 console.log(myAge);
 console.log(mySword, gun);
 
 // if the variables already declared
-let name = 'nero', 
+let name = 'nero',
     age = 12;
 
-({name , age } = obj) // using parenthesis because js take the left side as block; 
+({ name, age } = obj) // using parenthesis because js take the left side as block; 
 console.log(name, age);
+
+
+
+// Destructuring for Arrays
+let arr = [1, 2, 3];
+let [one, two, three] = arr;
+console.log(one, two, three);
+
+let testOne = 11, testTwo = 22, testThree = 33;
+
+[testOne, testTwo, testThree] = arr;
+console.log(testOne, testTwo, testThree);
